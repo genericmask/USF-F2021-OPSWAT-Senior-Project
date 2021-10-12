20e98f62c66ad56507a487d9e7df546e9f87cb44
Casey Feikema <feikema.casey@gmail.com>
2021-10-12 17:03:42 -0400

    moved all client files into their own folder

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+      <DesignPicture></DesignPicture>
+  </div>
+</template>
+
+<script>
+import DesignPicture from './components/DesignPicture'
+
+export default {
+  name: 'App',
+  components: {
+    DesignPicture
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
