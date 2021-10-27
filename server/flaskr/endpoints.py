ab5383678804745ce20d3422fc07acaaeccbae50
Casey Feikema <feikema.casey@gmail.com>
2021-10-27 10:40:22 -0400

    more stuff for the upload page

---
+++ server/flaskr/endpoints.py
@@ -0,0 +1,46 @@
+import os
+from flask import (
+    Blueprint, g, redirect, request, render_template, session, url_for, flash
+)
+from flaskr.db import get_db
+from werkzeug.utils import secure_filename
+from . import wificheck
+
+UPLOAD_FOLDER = '/home/pi/Desktop/SeniorProject/MVP/Upload'
+ALLOWED_EXTENSIONS = {'csv'}
+
+bp = Blueprint('endpoints', __name__, url_prefix = '/endpoints')
+
+def allowed_file(filename):
+        return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@bp.route('/upload', methods = ('GET', 'POST'))
+def upload():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('no file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            temp_file = wificheck.check_network()
+            file.save(os.path.join(UPLOAD_FOLDER, temp_file + '.csv'))
+            flash('Thank you')
+            return redirect(request.url)
+            #return redirect(url_for('upload_file', name=filename))
+            #return 'Thank you'
+
+    return render_template('home.html')
+    # return '''
+    # <!doctype html>
+    # <title> Upload Network csv File</title>
+    # <h1> Upload Network csv File</h1>
+    # <form method = post enctype=multipart/form-data>
+    #     <input type = file name = file>
+    #     <input type = submit value = Upload>
+    # </form>
+    # '''
