de39292f8e0f22db57ded9c9a3b16c010142508d
Casey Feikema <feikema.casey@gmail.com>
2021-10-27 20:42:58 -0400

    refactoring and framework for inserting endpoints into db from csv

--- server/flaskr/endpoints.py
+++ server/flaskr/endpoints.py
@@ -15,25 +15,45 @@ def allowed_file(filename):
         return '.' in filename and \
             filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def insert_endpoints(csv_file):
+    db = get_db()
+    # delete any endpoints that have the current network id
+    
+    # read the csv to get the endpoints
+    # insert the endpoints
+    try:
+        db.execute()
+        db.commit()
+    except:
+        return False
+        
+    return True
+
 @bp.route('/upload', methods = ('GET', 'POST'))
 def upload():
     if request.method == 'POST':
+        
+        error = None
+
         if 'file' not in request.files:
-            flash('no file part')
-            
-        file = request.files['file']
-        if file.filename == '':
-            flash('No selected file')
-            
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            # temp_file = wificheck.check_network()
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
-            flash('Thank you')
-            
-            return redirect(url_for('home'))
-            #return 'Thank you'
+            error = 'No file'
         else:
-            flash('Invalid file extension.')
+            file = request.files['file']
+            if file.filename == '':
+                error = 'No selected file'
+            elif not allowed_file(file.filename):
+                error = 'Invalid file extension.'
+            
+            if error is None:
+                insert_endpoints(file)
+                filename = secure_filename(file.filename)
+                # temp_file = wificheck.check_network()
+                file.save(os.path.join(UPLOAD_FOLDER, filename))
+                flash('Thank you')
+                
+                return redirect(url_for('home'))
+
+        flash(error)
+            
 
     return redirect(url_for('home'))
