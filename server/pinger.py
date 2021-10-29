8793494b42e082848436a85be472ec4ef11f0ec5
Chance Cornell <cacornell3@tampabay.rr.com>
2021-10-29 14:48:12 -0400

    I swear im happy

--- server/pinger.py
+++ server/pinger.py
@@ -1,22 +1,28 @@
+import csv
 import platform    # For getting the operating system name
 import os, subprocess  # For executing a shell command
 import datetime
 from alert import *
-from alt_db import get_endpoints
-#from wificheck import check_network
-#from TextAlert import sendText
+from wificheck import check_network
+from altdb import get_notification_settings
+from altdb import get_endpoints
+from TextAlert import sendText
 
 # Constants
+#CSV_FILENAME = 'endpointsIP1.csv'
 MAX_TIME = 100 # ms
 HEARRBEAT_DELAY = 900
 DELAY = 5 # sec
 
 def main():
-    start_time = -HEARRBEAT_DELAY
+    start_time = time.time()
     #elapsed_time = time.time() - start_time
     #print(elapsed_time)
     
-    pinger = Pinger(pingMaxTime=MAX_TIME, messageDelay=DELAY)
+    CSV_FILENAME = check_network() + '.csv'
+    #print(CSV_FILENAME)
+    #CSV_FILENAME = 'endpointsIP1.csv'
+    pinger = Pinger(filename=CSV_FILENAME, pingMaxTime=MAX_TIME, messageDelay=DELAY)
     
     #Run until the program is told otherwise
     print('\n\n-------------------------------------------\n\n')
@@ -25,14 +31,14 @@ def main():
             #Send out heartbeat alert every 15 minutes
             elapsed_time = time.time() - start_time
             if (HEARRBEAT_DELAY < elapsed_time):
-               #sendText('+18134465250', "Device is still alive") 
+               sendText(get_notification_settings()['phone_number'], "Device is still alive") 
                print("$$$$ Device is still alive $$$$", end = "\n\n")
                start_time = time.time() # reset time
 
             #Process the csv file and perform pings every 10 seconds unless a keyboard interrupt occurs
             #if (csv file change)
-            pinger.endpoints = get_endpoints()
-
+            #pinger.read_data(filename=CSV_FILENAME)
+            pinger.endpoints= get_endpoints()
             pinger.run_checker()
 
             print('\n\n-------------------------------------------\n\n')
@@ -44,10 +50,27 @@ def main():
 
 class Pinger:
 
-    def __init__(self, pingMaxTime=100, messageDelay=10):
+    def __init__(self, filename, pingMaxTime=100, messageDelay=10):
+        if filename ==  None:
+            self.endpoints = None
+            self.pingMaxTime = pingMaxTime
+            return
+        
         self.endpoints = get_endpoints()
         self.pingMaxTime = pingMaxTime
         self.alert = Alert(delay=messageDelay)
+
+    def read_data(self, filename):
+        '''
+            Load endpoints list from csv file
+            Returns endpoint list: {'ip': (str), 'accessible': (bool)}
+        '''
+        csvfile = open('/home/pi/Desktop/MVP/Upload/' + filename, 'r')
+        endpoints = [{'ip': ep[0], 'accessible': ep[1]=='TRUE'} for ep in csv.reader(csvfile)]
+        csvfile.close()
+        endpoints.pop(0)
+        self.endpoints = endpoints
+        return endpoints
 
     def ping(self, ip):
         '''
@@ -61,7 +84,7 @@ class Pinger:
         #response = subprocess.call(['ping', param, '3', '-w', str(self.pingMaxTime), ip])
         #response = subprocess.call(['ping', '-c', '1', '-W', '100', ip]) #MacOS
         # remove => "stdout=open(os.devnull, 'wb')" for ping results everytime
-        response = subprocess.call(['ping','-w', '1', ip], stdout=open(os.devnull, 'wb')) #Linux OS
+        response = subprocess.call(['ping','-w', '1', ip])#, stdout=open(os.devnull, 'wb')) #Linux OS
 
 
         # Return response
