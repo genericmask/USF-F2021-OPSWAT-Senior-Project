47603a2297cd1b1ca7aba282e1ddc261cd7bacfa
Casey Feikema <feikema.casey@gmail.com>
2021-11-03 16:28:06 -0400

    endpoints csv validator

--- server/flaskr/endpoints.py
+++ server/flaskr/endpoints.py
@@ -2,6 +2,7 @@ from flask import (
     Blueprint, g, redirect, request, render_template, session, url_for, flash
 )
 from flaskr.db import insert_endpoints
+from flaskr.validators.csvEndpointsValidator import csvEndpointsValidator
 
 ALLOWED_EXTENSIONS = {'csv'}
 
@@ -25,12 +26,20 @@ def upload():
                 error = 'No selected file'
             elif not allowed_file(file.filename):
                 error = 'Invalid file extension.'
-            
+
             if error is None:
-                insert_endpoints(file.read().decode("utf-8"))
-                flash('Thank you')
-                
-                return redirect(url_for('home'))
+                # Should be safe to read and decode file.
+                csv_string = file.read().decode("utf-8")
+
+                # Check for file content validity
+                error = csvEndpointsValidator(csv_string)
+
+                if error is None:
+                    # Insert if nothing was wrong
+                    insert_endpoints(csv_string)
+                    flash('Thank you')
+                    
+                    return redirect(url_for('home'))
 
         flash(error)
             
