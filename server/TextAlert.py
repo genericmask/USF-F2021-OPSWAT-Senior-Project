f029f678b0f93dffd9dc6eef9bd1f9bee2a4f860
Spencer McClanahan <MrZilldud49@gmail.com>
2021-11-08 11:20:43 -0500

    replaced old auth token issue

--- server/TextAlert.py
+++ server/TextAlert.py
@@ -2,7 +2,7 @@ from twilio.rest import Client
 
 def sendText(phoneNumber, body):
     if phoneNumber != '' and phoneNumber is not None:
-        client = Client("ACb4062d2c42e84a87eaef22235fcdcac1", "029b0411ceb8615d4d769f10b4bc6966")
+        client = Client("ACb4062d2c42e84a87eaef22235fcdcac1", "edbe014e2e889dd5cd199007f78919af")
         message = client.messages.create(
         to=phoneNumber, 
         from_="+12182749208",
