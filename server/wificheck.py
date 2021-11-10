8c1aade5a2ab03d1e440f59b12306fd5d84768f6
Casey Feikema <feikema.casey@gmail.com>
2021-11-10 08:39:25 -0500

    commented out for running on mac

--- server/wificheck.py
+++ server/wificheck.py
@@ -2,17 +2,17 @@ import subprocess
 
 
 def check_network():
-	wifi = subprocess.check_output(['iwlist', 'wlan0', 'scan'])
+	# wifi = subprocess.check_output(['iwlist', 'wlan0', 'scan'])
 
-	data = wifi.decode('utf-8')
+	# data = wifi.decode('utf-8')
 
-	marker = 0
+	# marker = 0
 
-	ssid = ""
+	# ssid = ""
 
-	for line in data.split():
-		if line.startswith("ESSID"):
-			ssid = line.split('"')[1]
-	return ssid
+	# for line in data.split():
+	# 	if line.startswith("ESSID"):
+	# 		ssid = line.split('"')[1]
+	return "ssid"
 
 #print (ssid)
