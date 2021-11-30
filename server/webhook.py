4bb7748ab1fee5698b814f58a6062b8b913e1a47
Casey Feikema <feikema.casey@gmail.com>
2021-11-30 08:25:57 -0500

    for webhook: handle post errors and send a json

--- server/webhook.py
+++ server/webhook.py
@@ -7,4 +7,7 @@ def webhook(webhook_url, ip, failure_description, start_datetime, end_datetime=N
         "end_datetime": end_datetime}
 
     if webhook_url != '':
-        requests.post(webhook_url, data=x)
+        try:
+            requests.post(webhook_url, json=x, timeout=5)
+        except requests.exceptions.RequestException as e:  # This will handle everything. TODO: Handle individual cases and give user feedback
+            print(e)
